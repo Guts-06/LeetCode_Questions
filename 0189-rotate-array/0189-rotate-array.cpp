7f7dac577ef7258a2d39c31e98bc142042293577
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-02 23:51:54 +0530

    Time: 0 ms (100%), Space: 29.4 MB (93.4%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+
+void reverse(vector<int>& nums,int start,int end){
+    while(start<end){
+        // nums[start]=nums[start]+nums[end];
+        // nums[end]=nums[start]-nums[end];
+        // nums[start]=nums[start]-nums[end];
+        //overflow risk above
+        swap(nums[start],nums[end]);
+        start++;
+        end--;
+    }
+}
+    void rotate(vector<int>& nums, int k) {
+        k=k%nums.size();
+        reverse(nums,0,nums.size()-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.size()-1);
+    }
+};
