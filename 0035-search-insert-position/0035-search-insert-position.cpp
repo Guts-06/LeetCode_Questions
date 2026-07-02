31c0c30629f90bc27f9989969d3341ce544930f6
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-03 00:06:43 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.82%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        int ans=e+1;
+        while(s<=e){
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]<target){
+                
+                s=mid+1;
+            }
+            else{
+                ans=mid;
+                e=mid-1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+    }
+};
