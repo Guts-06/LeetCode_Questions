5d53e885cb95e6c9c6fd8d5cfbf40be59ef0dd79
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-03 00:13:16 +0530

    Time: 0 ms (100%), Space: 87.2 MB (45.34%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        // unordered_map<int,int>mp;
+        int ten=0;
+        int five=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
+                five++;
+                continue;
+                
+            }
+            else if(bills[i]==10){
+                ten++;
+                if(five>=1){
+                    five--;
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if(bills[i]==20){
+                if(ten>=1 && five>=1){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
