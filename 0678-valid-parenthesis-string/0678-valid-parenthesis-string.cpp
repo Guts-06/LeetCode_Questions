d7eabcfb86a1c5aff0619aaab189c290e7bff1a0
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-03 00:13:19 +0530

    Time: 0 ms (100%), Space: 8.3 MB (26.41%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int>st;
+        stack<int>star;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(i);
+            }
+            else if(s[i]=='*'){
+                star.push(i);
+            }
+            else{
+                if(st.empty() && star.empty()){
+                return false;
+                }
+                else if(!st.empty()){
+                    st.pop();
+                }
+                else{
+                    star.pop();
+                } 
+            }
+        }
+        while(!st.empty()){
+            if(star.empty()){
+                return false;
+            }
+            else if(st.top()>star.top()){
+            return false;
+            }
+            st.pop();
+            star.pop();
+        }
+        return true;
+    }
+};
