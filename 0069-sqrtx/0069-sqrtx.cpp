22713f23051e9f97d12e73a3a87d24bdccde11f6
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-02 23:50:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (13.68%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int s= 0;
+        int e = x;
+        long long int mid = s + (e-s)/2;
+        int ans=-1;
+       
+        while(s<=e){
+            long long int square = mid*mid;
+            if(square==x){
+                return mid;
+            }
+            if(square<x){
+                ans = mid;
+                s=mid +1;
+            }
+            if(square>x){
+                e=mid-1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+        
+    }
+};
