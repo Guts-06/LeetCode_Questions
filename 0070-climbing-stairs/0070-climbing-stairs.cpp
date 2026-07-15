c88b99f2406497d66f314e55c2bb1f5fdf45a406
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-15 23:01:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.14%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<long long>dp(n+1);
+        
         if(n<=1){
             return 1;
         }
-        dp[0]=dp[1]=1;
+        int prev=1;
+        int prev2=1;
+        int curr=1;
         for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+            
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+
         }
-        return (int)dp[n];
+        return curr;
     }
 };
