728004b4d5e6cd2797057ba2f3a135daeb0347e2
Muhaimin Al Saboor Rizvi <179397614+Guts-06@users.noreply.github.com>
2026-07-19 15:41:35 +0530

    Time: 0 ms (100%), Space: 8.6 MB (94.04%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+       vector<int>freq(26,0);
+       vector<bool>vis(26,false);
+
+       for(auto &it:s){
+            freq[it-'a']++;
+       }
+       string ans;
+       for(auto &it:s){
+        freq[it-'a']--;
+        if(vis[it-'a']) continue;
+        while(ans.size()!=0 && ans.back()>it && freq[ans.back()-'a']>0){
+            vis[ans.back()-'a']=false;
+            ans.pop_back();
+        }
+        ans+=it;
+        vis[it-'a']=true;
+       }
+       return ans;
+    }
+};
